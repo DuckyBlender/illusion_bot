efd5a255b531268a34f5b1ab4c0baa94b4276c8f
Alan Klas <alan@alanklas.com>
2024-05-20 21:14:06 +0200

    first working version

---
+++ illusion.py
@@ -0,0 +1,37 @@
+import argparse
+from gradio_client import Client
+import sys
+import codecs
+
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach()) # utf8 output for rust subprocesses
+
+# Create the parser
+parser = argparse.ArgumentParser()
+
+# Add the arguments
+parser.add_argument('Prompt', metavar='Prompt', type=str, help='the prompt for the image')
+
+# Parse the arguments
+args = parser.parse_args()
+
+client = Client("https://ap123-illusiondiffusion.hf.space/--replicas/w3ut2/", verbose=False)
+result = client.predict(
+    # "https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png",  # filepath  in 'Input Illusion' Image component
+    "https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png",
+    args.Prompt,  # str  in 'Prompt' Textbox component
+    "low quality",  # str  in 'Negative Prompt' Textbox component
+    7.5,  # float (numeric value between 0.0 and 50.0) in 'Guidance Scale' Slider component
+    1.0,  # float (numeric value between 0.0 and 5.0) in 'Illusion strength' Slider component
+    0.0,  # float (numeric value between 0.0 and 1.0) in 'Start of ControlNet' Slider component
+    1.0,  # float (numeric value between 0.0 and 1.0) in 'End of ControlNet' Slider component
+    1.0,  # float (numeric value between 0.0 and 1.0) in 'Strength of the upscaler' Slider component
+    -1,  # float (numeric value between -1 and 9999999999) in 'Seed' Slider component
+    # "Euler",  # Literal['DPM++ Karras SDE', 'Euler']  in 'parameter_13' Dropdown component
+    "DPM++ Karras SDE", 
+    api_name="/inference"
+)
+
+# The result is a tuple, the first element is the image path
+image_path = result[0]
+
+print(image_path)
---
+++ src/main.rs
@@ -0,0 +1,80 @@
+use dotenv::dotenv;
+use log::info;
+use teloxide::{prelude::*, types::InputFile, utils::command::BotCommands};
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    pretty_env_logger::init();
+    info!("Starting command bot...");
+
+    let bot = Bot::from_env();
+
+    bot.set_my_commands(Command::bot_commands()).await.unwrap();
+
+    Command::repl(bot, answer).await;
+}
+
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+enum Command {
+    #[command(description = "generate an ilusion for the image reply with the specified prompt")]
+    Illusion(String),
+    #[command(description = "help command")]
+    Help,
+    #[command(description = "start command")]
+    Start,
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => {
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?;
+        }
+        Command::Start => {
+            bot.send_message(msg.chat.id, "Welcome to the Illusion Bot!")
+                .await?;
+        }
+        Command::Illusion(prompt) => {
+            // Invoke the python script with the argument prompt
+            let output = std::process::Command::new("cmd")
+                .arg("/C")
+                .arg("python")
+                .arg("illusion.py")
+                .arg(prompt.clone())
+                .output()
+                .expect("Failed to execute command");
+
+            info!("Status: {:?}", output.status);
+            // If the status is 0, then the command was successful and the output is a local path to the image
+            match output.status.code() {
+                Some(0) => {
+                    let output_str = String::from_utf8(output.stdout).unwrap();
+                    info!("Output: {:?}", output_str);
+                    // Load the file into memory
+                    let image = std::fs::read(output_str.trim()).unwrap();
+                    // Send the image as a photo
+                    bot.send_photo(msg.chat.id, InputFile::memory(image))
+                        .caption(prompt)
+                        .await?;
+                }
+                Some(code) => {
+                    info!("Command failed with code: {}", code);
+                    bot.send_message(msg.chat.id, format!("Command failed with code: {}", code))
+                        .await?;
+                }
+                None => {
+                    info!("Command failed with no code");
+                    bot.send_message(msg.chat.id, "Command failed with no code")
+                        .await?;
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
